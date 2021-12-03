22f341ae884c01ae229cf3048ddb2fc5eabd02b9
sam-klok <sklok11@gmail.com>
2021-12-02 23:50:47 -0700

    Initial commit

---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+I'm trying to broadcast messages in Angular app
+If we need to communicate between browser tabs, 
+let's use BroadcastChannel API 
+However will be chanllange to detect it (see last link)
+
+https://dev.to/rfornal/angular-communication-between-tabs-53cp
+
+https://stackoverflow.com/questions/62827047/angular-broadcastchannel-send-data-between-two-browser-tabs-property-does-not
+
+Detect changes
+https://www.positronx.io/understand-angular-change-detection-strategy/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'InterTabCart';
+  bc = new BroadcastChannel('cart_channel')
+  //@Input() cartCounter: number = 0;
+  cartCounter: number = 0;
+
+  constructor(private cd: ChangeDetectorRef) { 
+    let c = localStorage.getItem('cartCounter')
+    this.cartCounter = Number(c);
+  }
+  
+  ngOnInit(): void {
+    this.bc.onmessage = (event) => { 
+      if(event.data == 'update')
+      {
+        let c = localStorage.getItem('cartCounter')
+        this.cartCounter = Number(c);
+        console.log('message received ' + this.cartCounter);
+        this.cd.detectChanges();
+      }
+
+    }
+  }
+
+  handleAddToCart(){
+    this.cartCounter++;
+    console.log("adding item.. " + this.cartCounter);
+    localStorage.setItem('cartCounter', this.cartCounter.toString());
+    this.bc.postMessage('update');
+  }  
 }
+
